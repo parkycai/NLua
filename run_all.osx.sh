adcab5d1ac0f37ccd228b81f9adeb0d69105718d
Artum Tchachkin <aptem.tchachkin@gmail.com>
2018-01-30 09:16:51 +0200

    update submodules, travis ci config and lua53 name

--- run_all.osx.sh
+++ run_all.osx.sh
@@ -3,7 +3,7 @@ cd Core/KeraLua
 make -f Makefile.OSX
 xbuild KeraLua.Net45.sln /p:Configuration=Release
 cd ../../
-cp Core/KeraLua/external/lua/osx/lib/liblua52.dylib tests/liblua52.dylib
+cp Core/KeraLua/external/lua/osx/lib/liblua53.dylib tests/liblua53.dylib
 xbuild NLua.Net45.sln /p:Configuration=Release
 cd tests/
 export MONO_PATH="/Library/Frameworks/Mono.framework/Libraries/mono/4.5/"
--- run_all.win64.bat
+++ run_all.win64.bat
@@ -3,7 +3,7 @@ cd Core\KeraLua
 call Makefile.Win64.bat
 msbuild KeraLua.Net45.sln /p:Configuration=Release /p:DefineConstants="USE_DYNAMIC_DLL_REGISTER;WSTRING" /p:Platform="Any CPU"
 cd ..\..
-xcopy Core\KeraLua\external\lua\win64\bin64\*.dll tests\*.dll
+xcopy Core\KeraLua\external\lua\build\win64_rel_mt\bin64\*.dll tests\*.dll
 msbuild NLua.Net45.sln /p:Configuration=Release /p:DefineConstants="USE_DYNAMIC_DLL_REGISTER;WSTRING;LUA_CORE;CATCH_EXCEPTIONS" /p:Platform="Any CPU"
 cd tests/
 nunit-console NLuaTest.dll /xml=$1
